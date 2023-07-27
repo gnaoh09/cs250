2bdcdbbf0b4cc03c281598ebf0d9424a15d54a40
gnaoh09 <92804109+gnaoh09@users.noreply.github.com>
2023-07-27 22:53:37 +0700

    Function1.cpp

--- Function1.cpp
+++ Function1.cpp
@@ -3,35 +3,31 @@
 #include <string>
 using namespace std;
 int main(){
-    string data, fulldata;
+    //Print all information
+    string data, fulldata;    
     int data2;
     ifstream infile;
     infile.open("information.txt");
-
     cout << "Reading from the file" << endl;
     while (getline(infile, fulldata))
     {
         cout << fulldata << endl;
     }
-    // close the opened file.
     infile.close();
-
+    
+	//Count number of employee
     ifstream inputFile("information.txt");
     if (!inputFile.is_open()) {
         cerr << "Error opening the file." << endl;
         return 1;
     }
-
     int lineCount = 0;
     string line;
-
     while (getline(inputFile, line)) {
         lineCount++;
     }
-
     inputFile.close();
-
-    cout << "Number of lines in the file: " << lineCount - 7<< endl;
+    cout << "Number of lines in the file: " << lineCount - 7 << endl;  //The number of line without information is 7
 
     return 0;
 
