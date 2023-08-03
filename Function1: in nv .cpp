8b1d8360dce2617443c7de45a8d4f92cb131f630
gnaoh09 <92804109+gnaoh09@users.noreply.github.com>
2023-08-03 08:21:28 +0700

    Function1: in nv .cpp

--- Function1: in nv .cpp
+++ Function1: in nv .cpp
@@ -1,36 +1,17 @@
 #include<iostream>
+#include <sstream>
 #include<fstream> 
+#include <vector>
 #include <string>
+#include<algorithm>
 using namespace std;
+void Function1();
+
 int main(){
-    //Print all information
-    string data, fulldata;    
-    ifstream infile;
-    infile.open("information.txt");
-    while (getline(infile, fulldata))
-    {
-        cout << fulldata << endl;
-    }
-    infile.close();
-    
-	//Count number of employee
-    ifstream inputFile("information.txt");
-    if (!inputFile.is_open()) {
-        cerr << "Error opening the file." << endl;
-        return 1;
-    }
-    int lineCount = 0;
-    string line;
-    while (getline(inputFile, line)) {
-        lineCount++;
-    }
-    inputFile.close();
-    cout << "Total number of employees is: " << lineCount - 7 << endl;  //The number of line without information is 7
+
+    Function1();
 
     return 0;
-
-
-
 }
 
 
@@ -38,4 +19,49 @@ int main(){
 
 
 
-	
+
+
+
+
+
+void Function1(){ 
+    int i = 0;
+    string data, line, token;                                                      //Print all information
+    ifstream infile;
+    infile.open("information.txt");
+    while (getline(infile, line))
+    {
+        if (i >= 3){
+            vector<string> components;
+            stringstream ss(line);
+            while (ss >> token)
+            {
+                components.push_back(token);
+            };
+
+            for (int j = 0; j < components.size(); j++)
+            {
+                replace(components[j].begin(), components[j].end(),'_',' ');
+                cout << components[j] << "   ";
+            };
+            cout << endl;
+        };
+        i++;
+    }
+    infile.close();
+
+	                                                                        
+    ifstream inputFile("information.txt");                                  //Count number of employee
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file." << endl;
+     
+    }
+    int lineCount = 0;
+   
+    while (getline(inputFile, line)) {
+        lineCount++;
+    }
+    inputFile.close();
+    cout << "Total number of employees is: " << lineCount - 9 << endl;  //The number of line without information is 7
+}
+    
