2385a2305d01c13e52c56f4f2cf9e9ad659fc676
gnaoh09 <92804109+gnaoh09@users.noreply.github.com>
2023-08-07 09:13:28 +0700

    Add files via upload

---
+++ Function5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include "Function5.h"
+using namespace std;
+
+int main()
+{
+    changeFunction();
+
+    return 0;
+}
+
+
+
+
---
+++ Function6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <random>
+#include <algorithm>
+#include "Function6.h"
+using namespace std;
+int main()
+{
+    runFunction();
+    return 0;
+}
+
+
